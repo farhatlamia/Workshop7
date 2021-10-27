5da57a3f194e402b22727f982022b43a06313d3b
Farhat Lamia <41986330+farhatlamia@users.noreply.github.com>
2021-10-27 13:34:53 -0500

    Add files via upload

---
+++ bandit.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+import simplejson as json
+
+#command = "cd Downloads; cd ML-CODE; find . | xargs bandit -f json"
+path = './ML-CODE'
+os.chdir(path)
+
+command = "find . | xargs bandit -f json"
+ret = subprocess.run(command, capture_output=True, shell=True)
+x = ret.stdout.decode()
+#print(x)
+
+main_list = json.loads(x)
+result= main_list["results"]
+print(result)
+
+mylist = []
+
+for i in result:
+    temp= {}
+    temp["FILE"] = path+ i["filename"]
+    alerts = {}
+    alerts["SEVERITY"] = i["issue_severity"]
+    alerts["CONFIDENCE"]= i["issue_confidence"]
+    alerts["NAME"]= i["test_name"]
+    alerts["LINE_NO"]= i["line_number"]
+    temp["ALERTS"] = alerts
+    mylist.append(temp)
+
+with open("sample.txt", "w") as fhandle:
+    json.dump(mylist, fhandle, indent=4)
+
+
+
+
+
